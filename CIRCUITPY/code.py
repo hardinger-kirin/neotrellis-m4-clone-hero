248b04b5045dcd18b8894ad0e48a095f462fa9ee
Kirin <89322661+hardinger-kirin@users.noreply.github.com>
2026-04-15 17:01:24 -0700

    * Make idle colors more vibrant * Add pressed colors (muted)

--- CIRCUITPY/code.py
+++ CIRCUITPY/code.py
@@ -8,9 +8,14 @@ keyboard = Keyboard(usb_hid.devices)
 
 COLOR_GREEN = (0, 100, 0)
 COLOR_RED = (100, 0, 0)
-COLOR_YELLOW = (50, 50, 0)
+COLOR_YELLOW = (100, 100, 0)
 COLOR_BLUE = (0, 0, 100)
-COLOR_ORANGE = (75, 25, 0)
+COLOR_ORANGE = (100, 50, 0)
+COLOR_PRESSED_GREEN = (0, 50, 0)
+COLOR_PRESSED_RED = (50, 0, 0)
+COLOR_PRESSED_YELLOW = (50, 50, 0)
+COLOR_PRESSED_BLUE = (0, 0, 50)
+COLOR_PRESSED_ORANGE = (50, 25, 0)
 COLOR_IDLE = (20, 20, 20)
 COLOR_OFF = (0, 0, 0)
 
@@ -22,7 +27,7 @@ KEY_MAP = {
     (4, 0): Keycode.L,
 }
 
-COLOR_MAP = {
+IDLE_COLOR_MAP = {
     (0, 0): COLOR_GREEN,
     (1, 0): COLOR_RED,
     (2, 0): COLOR_YELLOW,
@@ -30,8 +35,16 @@ COLOR_MAP = {
     (4, 0): COLOR_ORANGE,
 }
 
-for pos in COLOR_MAP:
-    trellis.pixels[pos] = COLOR_MAP[pos]
+PRESSED_COLOR_MAP = {
+    (0, 0): COLOR_PRESSED_GREEN,
+    (1, 0): COLOR_PRESSED_RED,
+    (2, 0): COLOR_PRESSED_YELLOW,
+    (3, 0): COLOR_PRESSED_BLUE,
+    (4, 0): COLOR_PRESSED_ORANGE,
+}
+
+for pos in IDLE_COLOR_MAP:
+    trellis.pixels[pos] = IDLE_COLOR_MAP[pos]
 
 pressed = set()
 
@@ -44,11 +57,11 @@ while True:
     for pos in just_pressed:
         if pos in KEY_MAP:
             keyboard.press(KEY_MAP[pos])
-            trellis.pixels[pos] = COLOR_IDLE
+            trellis.pixels[pos] = PRESSED_COLOR_MAP[pos]
 
     for pos in just_released:
         if pos in KEY_MAP:
             keyboard.release(KEY_MAP[pos])
-            trellis.pixels[pos] = COLOR_MAP[pos]
+            trellis.pixels[pos] = IDLE_COLOR_MAP[pos]
 
     pressed = current
